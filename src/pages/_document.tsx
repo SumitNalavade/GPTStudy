2e0b974fb70c8ede1ad2750b00e42c8fbac879d7
SumitNalavade <vs.nalavade2003@gmail.com>
2023-06-06 18:37:49 -0500

    Add open graph tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>GPT Study</title>
+        <meta property="og:title" content="GPT Study" key="title" />
+        <meta property="og:description" content="Generate study material with the GPT AI" key="description" />
+        <meta property="og:image" content="/home.png" key="description" />
+      </Head>
       <body>
         <Main />
         <NextScript />
